ba3c866388db2b6ac7a191f32ea17dc068b301d2
Benjamin Bachmann <bbachmann@bloopark.de>
2015-07-28 11:19:24 +0200

    [ADD] added license information and import improvement

--- __openerp__.py
+++ __openerp__.py
@@ -20,28 +20,20 @@
 ##############################################################################
 {
     'name': "bp_8_0_social_media_for_res_partner",
-
-    'summary': """
-    social media links for the res_parter modul""",
-
-    'description': """
-    social media links for the res_parter modul""",
-
+    'summary': """social media links for the res_parter modul""",
+    'description': """social media links for the res_parter modul""",
     'author': "BLOOPARK SYSTEMS GMBH. & CO. KG",
     'website': "http://www.bloopark.de",
-
+    'license': 'AGPL-3',
     'category': 'social_media',
     'version': '0.1',
 
-    # any module necessary for this one to work correctly
     'depends': [
         'base',
         'hr'
     ],
 
-    # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
         'views/res_partner_viev.xml'
     ],
 }
